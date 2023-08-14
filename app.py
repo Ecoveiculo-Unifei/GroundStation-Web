7f32354d51b2d9ed658f40873a2c1b9d6b30e1f9
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2023-08-14 14:39:47 -0300

    update config and application

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 
 
 st.warning("Aplicação ainda não implementada")
-st.markdown("[![Click me](/static/Logo_Eco.png)](https://www.instagram.com/ecoveiculounifei/)")
+st.markdown("[![Click me](./static/Logo_Eco.png)](https://www.instagram.com/ecoveiculounifei/)")
