c94f059f5f70f0497b11ef87afefdfff90685d08
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2023-08-14 14:58:08 -0300

    Atualiza app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed",
     menu_items={
-        'Sobre': "# O ecoveículo, acesse nosso instagram: https://www.instagram.com/ecoveiculounifei/"
+        'About': "# O ecoveículo, acesse nosso instagram: [![Click me](app/static/Logo_Eco.png)](https://www.instagram.com/ecoveiculounifei/)"
     }
 )
 
@@ -14,7 +14,4 @@ st.warning("Aplicação ainda não implementada")
 st.markdown("[![Click me](app/static/Logo_Eco.png)](https://www.instagram.com/ecoveiculounifei/)")
 
 with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
+    st.markdown("#Tenha um bom dia")
