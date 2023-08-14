b3e0399ce98a226f1f9cf9dbc787f9228f39e1b3
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2023-08-14 14:52:12 -0300

    Modifica o App

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-
+st.set_page_config(
+    page_title="Evoceículo",
+    page_icon="static/Logo_Eco.png",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Sobre': "# O ecoveículo, acesse nosso instagram: https://www.instagram.com/ecoveiculounifei/"
+    }
+)
 
 st.warning("Aplicação ainda não implementada")
-st.markdown("[![Click me](./static/Logo_Eco.png)](https://www.instagram.com/ecoveiculounifei/)")
+st.markdown("[![Click me](app/static/Logo_Eco.png)](https://www.instagram.com/ecoveiculounifei/)")
+
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
